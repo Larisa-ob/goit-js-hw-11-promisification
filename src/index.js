d5d4a06d24164acb9ff1e976b7acf2042db2f5a6
Larisa-ob <66275364+Larisa-ob@users.noreply.github.com>
2021-01-11 23:39:09 +0200

    work 3 all

--- src/index.js
+++ src/index.js
@@ -1,18 +1,10 @@
-console.log('start....');
+import delay from './promise1';
+import toggleUserState from './promise2';
+import makeTransaction from './promise3';
+console.log(' start ...');
 
-const promise = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        console.log('from timer ...');
-        resolve(logger());
-    }, 2000);
-    const logger = time => {
-        console.log(`Resolved after ${time}ms`);
-    };
-});
-//delay(2000).then(logger);
-//promise.then(const delay = time => { logger(time); });
-console.log('end ...');
-/*
- * then не выполнится так как в функции fn, внутри new Promise(fn),
- * был вызван reject(). А catch как раз выполнится через 2 секунды
- */
+setTimeout(function () {
+    console.log('setTimeout');
+}, 2000);
+
+Promise.resolve().then(toggleUserState).then(makeTransaction).then(delay);
