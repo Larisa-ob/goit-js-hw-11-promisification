a4e56e3ffae268d9b7238b9f032740772605d3bd
Larisa-ob <66275364+Larisa-ob@users.noreply.github.com>
2021-01-14 10:48:48 +0200

    promise1

--- goit-js-hw-11-promisification/src/index.js
+++ goit-js-hw-11-promisification/src/index.js
@@ -1,10 +1,11 @@
 import delay from './promise1';
-import toggleUserState from './promise2';
-import makeTransaction from './promise3';
+//import toggleUserState from './promise2';
+//import makeTransaction from './promise3';
 console.log(' start ...');
 
 setTimeout(function () {
-    console.log('setTimeout');
+  console.log('setTimeout');
 }, 2000);
 
-Promise.resolve().then(toggleUserState).then(makeTransaction).then(delay);
+//Promise.resolve().then(toggleUserState).then(makeTransaction).then(delay);
+Promise.resolve().then(delay);
